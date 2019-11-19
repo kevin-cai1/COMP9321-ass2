98696b76139787b3899387fe75a4400e3ba76c3e
tonnnyk <8369049+tonnnyK@users.noreply.github.com>
2019-11-19 18:30:02 +1100

    initial clean of histories

--- fuel_model/price_history.py
+++ fuel_model/price_history.py
@@ -17,15 +17,22 @@ def _extract_date(name: str) -> datetime:
     except ValueError:
         raise ValueError('Invalid name: {}'.format(name))
 
+def _clean(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.dropna(how='all')
+    df = df.interpolate(method='pad')
+    return df
+
 # Returns data from monthly excel file at url
 def _read_month(url: str, name: str) -> pd.DataFrame:
     FORMAT_CHANGE_DATE = datetime(2017, day=1, month=7)
     if _extract_date(name) < FORMAT_CHANGE_DATE:
         new_df = pd.read_excel(url)
     # From 7/2017 format changed to add a title in 1st row
+    # TODO: check format change where 2nd row is empty
     else:
         new_df = pd.read_excel(url, skiprows=1)
 
+    #return _clean(new_df)
     return new_df
 
 # Returns combined monthly price histories
@@ -50,3 +57,4 @@ if __name__ == "__main__":
     df = _read_month('https://data.nsw.gov.au/data/dataset/a97a46fc-2bdd-4b90-ac7f-0cb1e8d7ac3b/resource/efcbe322-d35f-4fba-8f8b-b073035cfad5/download/price_history_checks_oct2019.xlsx', 'FuelCheck Price History October 2019')
     print(df.head(5).to_string())
     print(df.tail(5).to_string())
+    print('types: {}'.format(df.dtypes))
