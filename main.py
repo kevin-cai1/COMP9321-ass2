8ab3d000c517e25490e140c6ee5d335691fb2390
philipxdu <philipxdu@gmail.com>
2019-11-20 00:48:11 +1100

    worked on home page and added about page

--- main.py
+++ main.py
@@ -21,5 +21,10 @@ def home():
 
     return render_template("home.html", form=form)
 
+def about():
+
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
