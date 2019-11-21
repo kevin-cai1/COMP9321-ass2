f03f2b597d2708ae4fe9438971b4586bb79187f3
Tony Kang <8369049+tonnnyK@users.noreply.github.com>
2019-11-21 21:46:55 +1100

    Changed read to take in a range of dates

--- fuel_model/price_history.py
+++ fuel_model/price_history.py
@@ -1,38 +1,43 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+import datetime
 import requests
 
 # Extracts date from data's name.
 # name format: Fuelcheck Price History MONTH YEAR[.xlsx]
-def _extract_date(name: str) -> datetime:
+def _extract_date(name: str) -> datetime.date:
     # some names have .xlsx in them
+    name = name.strip()
     split = name.partition('.xlsx')[0].split(' ')
     if len(split) < 2:
         raise ValueError('Invalid name: {}'.format(name))
-    month = split[-2][:3]
+    month = split[-2]
     year = split[-1]
 
     try:
-        return datetime.strptime(month + '-' + year, '%b-%Y')
+        return datetime.datetime.strptime(month[:3] + '-' + year, '%b-%Y').date()
     except ValueError:
         raise ValueError('Invalid name: {}'.format(name))
 
 # Sets first row as column labels
-def _set_header(df: pd.DataFrame):
+def _set_header(df: pd.DataFrame) -> pd.DataFrame:
     df.rename(columns=df.iloc[0], inplace=True)
     df.drop(index=df.index[0], inplace=True)
+    df.columns = df.columns.str.strip()
+    return df
 
-def _set_dtypes(df: pd.DataFrame):
+def _set_dtypes(df: pd.DataFrame) -> pd.DataFrame:
     df['Price'] = pd.to_numeric(df['Price'], downcast='float')
     df['PriceUpdatedDate'] = (
             pd.to_datetime(df['PriceUpdatedDate'], format='%d/%m/%Y %I:%M:%S %p'))
     return df
 
 def _clean(df: pd.DataFrame) -> pd.DataFrame:
-    # Drop row all nans e.g. some sheets have whitespace at start of file
-    df.dropna(how='all', inplace=True)
-    _set_header(df)
+    # Some sheets have 3 'valid' NaNs (8 cols total) that
+    # must be inferred i.e. FuelCode, PriceUpdatedDate, Price
+    # Some sheets have useless rows e.g. at start
+    df.dropna(thresh=3, inplace=True)
+    df = _set_header(df)
 
     # Data from excel sheets sometimes has empty space where there is replication,
     # but with FuelCode, PriceUpdatedDate, Price intact
@@ -46,44 +51,37 @@ def _clean(df: pd.DataFrame) -> pd.DataFrame:
     # Clean up any rows that haven't been interpolated
     df.dropna(how='any', inplace=True)
 
-    _set_dtypes(df)
+    df = _set_dtypes(df)
 
     return df
 
 # Returns data from monthly excel file at url
-def _read_month(url: str, name: str) -> pd.DataFrame:
-    date = _extract_date(name)
-    FORMAT_CHANGE_DATE = datetime(2017, day=1, month=7)
-    args = {'io': url, 'header': None}
-    if date < FORMAT_CHANGE_DATE:
-        new_df = pd.read_excel(**args)
-    # From 7/2017 format changed to add a title in 1st row
-    else:
-        args['skiprows'] = 1
-        new_df = pd.read_excel(**args)
-
+def _read_month(url: str) -> pd.DataFrame:
+    new_df = pd.read_excel(io=url, header=None)
     return _clean(new_df)
 
-# Returns combined monthly price histories
-def read() -> pd.DataFrame:
-    URL = 'https://data.nsw.gov.au/data/api/3/action/package_show?id=a97a46fc-2bdd-4b90-ac7f-0cb1e8d7ac3b'
-    resp = requests.get(URL)
+# Returns combined monthly price histories starting from 'start' and until
+# 'end', both inclusive
+def read(start: datetime.date, end: datetime.date) -> pd.DataFrame:
+    META_URL = 'https://data.nsw.gov.au/data/api/3/action/package_show?id=a97a46fc-2bdd-4b90-ac7f-0cb1e8d7ac3b'
+    resp = requests.get(META_URL)
     resp.raise_for_status()
 
     resources = (resp.json())['result']['resources']
     DATA_OFFSET = 4
-    df = _read_month(resources[DATA_OFFSET]['url']
-                     , resources[DATA_OFFSET]['name'])
-    for resource in resources[DATA_OFFSET + 1:]:
-        df = df.append(_read_month(resource['url'], resource['name'])
-                       , ignore_index=True
-                       , sort=True)
+    df = pd.DataFrame()
+    for resource in resources[DATA_OFFSET:]:
+        date = _extract_date(resource['name'])
+        if (date >= start
+        and date <= end):
+            df = df.append(_read_month(resource['url'])
+                           , ignore_index=True
+                           , sort=True)
 
     return df
 
 if __name__ == "__main__":
-    #df = read()
-    df = _read_month('https://data.nsw.gov.au/data/dataset/a97a46fc-2bdd-4b90-ac7f-0cb1e8d7ac3b/resource/efcbe322-d35f-4fba-8f8b-b073035cfad5/download/price_history_checks_oct2019.xlsx', 'FuelCheck Price History October 2019')
+    df = read(start=datetime.date(2019, 10, 1), end=datetime.date(2019, 10, 1))
     print(df.head(5).to_string())
     print(df.tail(5).to_string())
     print('types: {}'.format(df.dtypes))
