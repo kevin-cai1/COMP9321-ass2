78423c7b6d485061da019ec1379a589a4c377066
Blake Dutton <blakedut@gmail.com>
2019-11-25 17:00:57 +1100

    Add new Station component

---
+++ ui/src/components/Station.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ result.Station_Name }}</h2>
+        <p><b>Address:</b> {{ result.Station_Address }}</p>
+        <p><b>Fuel Type:</b> {{ result.Fuel_Type }}</p>
+        <br/>
+        <b-table striped hover :items="result.Prices"></b-table>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Station',
+    props: {
+        result: Object
+    }
+}
+</script>
